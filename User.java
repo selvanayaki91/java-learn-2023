45d8387602a39ff4bb27a4dd0b4aab8298303a18
selvanayaki91 <selvarjunan91@gmail.com>
2023-01-29 02:58:01 -0500

    hfvuhb

---
+++ User.java
@@ -0,0 +1,13 @@
+class User
+{
+
+public static void main(String[] args)
+
+{
+Whatsapp whatsapp = new Whatsapp();
+whatsapp.send_msg();
+//whatsapp.collect_data();
+System.out.println(whatsapp.profit);
+}
+
+}
