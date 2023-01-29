154b2441e5c4282f812f8f0c5ce5405a77223146
selvanayaki91 <selvarjunan91@gmail.com>
2023-01-29 05:20:27 -0500

    coomiited cat code

---
+++ Cat.java
@@ -0,0 +1,15 @@
+package tamilnadu.salem;
+import tamilnadu.chennai.Dog;
+
+class Cat extends Dog
+{
+
+public static void main(String[] args)
+{
+
+Cat catobj = new Cat();
+catobj.eating();
+System.out.println("The dog age is " + catobj.age);
+}
+
+}
